24b474fcc9ba08a04d3006c97b143b7f4ca48e47
start50-dev <start50dev@outlook.com>
2024-06-07 20:34:05 +0700

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,36 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface ComponentsLink extends Schema.Component {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    url: Attribute.String;
+    text: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    description: '';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    images: Attribute.Media<'images'>;
+    link: Attribute.Component<'components.link'>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'components.link': ComponentsLink;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
+}
